489e5859e0875862b61cca16f661960ffc54f2fd
Toledo <toledo.programmer@hotmail.com>
2025-09-03 07:53:13 -0600

    Base de la aplicación de angular. Http client configurado.

---
+++ APP/src/app/app.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { response } from 'express';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App')
+
+  ngOnInit(): void {
+    this.http.get("https://localhost/5001/api/members").subscribe({
+      next : response => console.log(response),
+      error: error=> console.error(error),
+      complete: ()=> console.log("Complete")
+    })
+  }
+}
