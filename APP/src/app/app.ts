c8cc642813a40c637e70b8e87beed70ab0d1e987
Toledo <toledo.programmer@hotmail.com>
2025-09-04 09:06:02 -0600

    Se añadió el environment.ts para obtener el API URL

--- APP/src/app/app.ts
+++ APP/src/app/app.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { response } from 'express';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +13,9 @@ import { response } from 'express';
 export class App implements OnInit{
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App')
-
+  
   ngOnInit(): void {
-    this.http.get("https://localhost/5001/api/members").subscribe({
+    this.http.get(`${environment.apiUrl}/api/members`).subscribe({
       next : response => console.log(response),
       error: error=> console.error(error),
       complete: ()=> console.log("Complete")
