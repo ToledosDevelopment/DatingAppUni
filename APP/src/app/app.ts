4bad82af75761c6f1e6e05ed66187bd2c99b4f74
Toledo <toledo.programmer@hotmail.com>
2025-09-11 12:27:54 -0600

    Mostramos los usuarios

--- APP/src/app/app.ts
+++ APP/src/app/app.ts
@@ -3,6 +3,8 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { response } from 'express';
 import { environment } from '../environments/environment';
+import { lastValueFrom } from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,19 @@ import { environment } from '../environments/environment';
 export class App implements OnInit{
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App')
+  protected members = signal<any>([]);
   
-  ngOnInit(): void {
-    this.http.get(`${environment.apiUrl}/api/members`).subscribe({
-      next : response => console.log(response),
-      error: error=> console.error(error),
-      complete: ()=> console.log("Complete")
-    })
+  async ngOnInit(): Promise<void> {
+    this.members.set(await this.getMembers())
+  }
+
+  async getMembers(): Promise<Object>{
+    try{
+      return lastValueFrom(this.http.get(`${environment.apiUrl}/api/members`))
+    }
+    catch(error){
+      console.error(error);
+      throw error;
+    }
   }
 }
