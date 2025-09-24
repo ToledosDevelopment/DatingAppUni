a1093c84c3cac2bd19afe36a26b53adab7301bf8
Toledo <toledo.programmer@hotmail.com>
2025-09-24 07:27:59 -0600

    Se agregó la home page

--- APP/src/app/app.ts
+++ APP/src/app/app.ts
@@ -4,11 +4,11 @@ import { environment } from '../environments/environment';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from './layout/nav/nav';
 import { AccountService } from '../core/services/account-service';
-
+import { Home } from '../features/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [Nav],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
