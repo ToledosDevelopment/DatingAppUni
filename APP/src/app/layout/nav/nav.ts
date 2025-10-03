02fb08c99065e3f83650926739c22b3da6a2245c
Toledo <toledo.programmer@hotmail.com>
2025-10-03 08:28:16 -0600

    Añadimos las rutas protegidas al guard

--- APP/src/app/layout/nav/nav.ts
+++ APP/src/app/layout/nav/nav.ts
@@ -2,6 +2,8 @@ import { Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastService } from '../../../core/services/toast-service'; 
+
 
 @Component({
   selector: 'app-nav',
@@ -13,14 +15,19 @@ export class Nav {
   protected creds: any = {};
   protected accountService = inject(AccountService);
   private router = inject(Router);
+  private toast = inject(ToastService);
+
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
       next: response => {
         this.router.navigateByUrl("/members");
         this.creds = {};
+        this.toast.success("Logged in!")
       },
-      error: error => alert(error.message)
+      error: error => {
+        this.toast.error(error.error);
+      }
     });
   }
 
