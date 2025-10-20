fab3310720caec97ec7eb17fd99e21650d4a939c
Toledo <toledo.programmer@hotmail.com>
2025-10-20 07:48:03 -0600

    Using members repository on members controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,25 +1,35 @@
+using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers;
 [Authorize]
-public class MembersController(AppDbContext context) : BaseApiController
+public class MembersController(IMembersRepository membersRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet]
-    public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+    public async Task<IReadOnlyList<Member>> GetMembers()
     {
-        return Ok(context.Users.ToList());
+        return await membersRepository.GetMembersAsync();
     }
 
-    [AllowAnonymous]
     [HttpGet("{id}")]
-    public ActionResult<AppUser> GetMember(string id)
+    public async Task<ActionResult<Member>> GetMember(string id)
     {
-        var member = context.Users.Find(id);
-        if (member == null) return NotFound();
+        var member = await membersRepository.GetMemberAsync(id);
 
-        return Ok(member);
+        if (member == null)
+        {
+            return NotFound();
+        }
+        return member;
     }
+
+    [HttpGet("{id}/photos")]
+    public async Task<ActionResult<IReadOnlyList<Member>>> GetPhotos(string id)
+    {
+        return Ok(await membersRepository.GetPhotosAsync(id));
+    }
+
 }
