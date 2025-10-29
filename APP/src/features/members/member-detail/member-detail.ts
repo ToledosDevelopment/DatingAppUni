219f1ac52f07139d24fd0de21862833a00270d6b
Toledo <toledo.programmer@hotmail.com>
2025-10-29 08:55:41 -0600

    Add member resolver

--- APP/src/features/members/member-detail/member-detail.ts
+++ APP/src/features/members/member-detail/member-detail.ts
@@ -1,25 +1,26 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
-import { MembersService } from '../../../core/services/members-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../types/member';
-import { filter, Observable } from 'rxjs';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
 export class MemberDetail implements OnInit {
-  private membersService = inject(MembersService);
   private route = inject(ActivatedRoute);
   private router = inject(Router);
-  protected member$?: Observable<Member>;
   protected title = signal<string | undefined>("Profile");
+  protected member = signal<Member | undefined>(undefined);
+
 
   ngOnInit(): void {
-    this.member$ = this.loadMember();
+    this.route.data.subscribe({
+        next: data => this.member.set(data["member"])
+    });
+
     this.title.set(this.route.firstChild?.snapshot?.title);
 
     this.router.events.pipe(
@@ -29,15 +30,5 @@ export class MemberDetail implements OnInit {
         this.title.set(this.route.firstChild?.snapshot?.title);
       }
     });
-  }
-
-  loadMember(): Observable<Member> | undefined {
-    const id = this.route.snapshot.paramMap.get("id");
-
-    if (id) {
-      return this.membersService.getMember(id);
-    }
-
-    return;
   }
 }
