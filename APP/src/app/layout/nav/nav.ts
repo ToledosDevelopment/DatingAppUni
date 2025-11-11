d9045c407a7b228d090d430c70b284e58a052c01
Toledo <toledo.programmer@hotmail.com>
2025-11-11 08:00:01 -0600

    Add theme manager

--- APP/src/app/layout/nav/nav.ts
+++ APP/src/app/layout/nav/nav.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastService } from '../../../core/services/toast-service'; 
+import { themes } from '../Theme';
 
 
 @Component({
@@ -16,7 +17,22 @@ export class Nav {
   protected accountService = inject(AccountService);
   private router = inject(Router);
   private toast = inject(ToastService);
+  protected selectedTheme = signal<string>(localStorage.getItem("theme") || "light");
+  protected themes = themes;
 
+  ngOnInit(): void {
+    document.documentElement.setAttribute("data-theme", this.selectedTheme());
+  }
+
+  handleSelectedTheme(theme: string) {
+    this.selectedTheme.set(theme);
+    localStorage.setItem("theme", theme);
+    document.documentElement.setAttribute("data-theme", theme);
+    const elem = document.activeElement as HTMLDivElement;
+    if (elem) {
+      elem.blur();
+    }
+  }
 
   login(): void {
     this.accountService.login(this.creds).subscribe({
