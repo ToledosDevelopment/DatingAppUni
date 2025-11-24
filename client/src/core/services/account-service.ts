ff80061b8c757ec15e4c801ea11572ffeeea4026
Toledo <toledo.programmer@hotmail.com>
2025-11-24 10:42:55 -0600

    Squash merge de cambios en parcial 3 a development

---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,45 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable, tap } from 'rxjs';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  baseUrl = `${environment.apiUrl}/api/`;
+  currentUser = signal<User | null>(null);
+
+  register(creds: RegisterCreds) : Observable<User>{
+    return this.http.post<User>(this.baseUrl + "account/register", creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  login(creds: LoginCreds): Observable<User> {
+    return this.http.post<User>(this.baseUrl + "account/login", creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem("user", JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
+    localStorage.removeItem("user");
+    this.currentUser.set(null);
+  }
+}
---
+++ client/src/features/members/member-detail/member-detail.ts
@@ -0,0 +1,35 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Member } from '../../../types/member';
+import { filter } from 'rxjs';
+import { AgePipe } from '../../../core/pipes/age-pipe';
+
+@Component({
+  selector: 'app-member-detail',
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, AgePipe],
+  templateUrl: './member-detail.html',
+  styleUrl: './member-detail.css'
+})
+export class MemberDetail implements OnInit {
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  protected title = signal<string | undefined>("Profile");
+  protected member = signal<Member | undefined>(undefined);
+
+
+  ngOnInit(): void {
+    this.route.data.subscribe({
+        next: data => this.member.set(data["member"])
+    });
+
+    this.title.set(this.route.firstChild?.snapshot?.title);
+
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe({
+      next: () => {
+        this.title.set(this.route.firstChild?.snapshot?.title);
+      }
+    });
+  }
+}
