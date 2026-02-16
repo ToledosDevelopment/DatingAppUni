cdf132635765d9728057e7b845843e367d976281
Toledo <toledo.programmer@hotmail.com>
2026-02-15 20:17:27 -0600

    Add edit profile button

--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -6,10 +6,10 @@ import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
-selector: 'app-member-photos',
+  selector: 'app-member-photos',
   imports: [AsyncPipe],
-templateUrl: './member-photos.html',
-styleUrl: './member-photos.css'
+  templateUrl: './member-photos.html',
+  styleUrl: './member-photos.css'
 })
 export class MemberPhotos {
   private membersService = inject(MembersService);
@@ -24,7 +24,7 @@ export class MemberPhotos {
   }
 
   get photoMocks() {
-    return Array.from({ length: 20 }, (_, i) => ({
+    return Array.from({ length: 0 }, (_, i) => ({
       url: "./user.jpg"
     }));
   }
--- client/src/features/members/member-profile/member-profile.ts
+++ client/src/features/members/member-profile/member-profile.ts
@@ -2,16 +2,18 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../../types/member';
 import { DatePipe } from '@angular/common';
+import { MembersService } from '../../../core/services/members-service';
 
 @Component({
-selector: 'app-member-profile',
-imports: [DatePipe],
-templateUrl: './member-profile.html',
-styleUrl: './member-profile.css'
+  selector: 'app-member-profile',
+  imports: [DatePipe],
+  templateUrl: './member-profile.html',
+  styleUrl: './member-profile.css'
 })
 export class MemberProfile implements OnInit {
   private route = inject(ActivatedRoute);
   protected member = signal<Member | undefined>(undefined);
+  protected readonly membersService = inject(MembersService);
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
